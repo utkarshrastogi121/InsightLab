39a634a8bc1343fd6ee4226784d4d046697066dc
Utkarsh Rastogi <utkarsh.2023ug1077@iiitranchi.ac.in>
2026-01-11 20:46:24 +0530

    news categories and source added

---
+++ backend/src/constants/categories.ts
@@ -0,0 +1,10 @@
+export const NEWS_CATEGORIES = {
+  TECHNOLOGY: "Technology",
+  AI: "AI",
+  BUSINESS: "Business",
+  SPORTS: "Sports",
+  WORLD: "World",
+  POLITICS: "Politics",
+  STARTUPS: "Startups",
+  HEALTH: "Health",
+} as const;
---
+++ backend/src/constants/sources.ts
@@ -0,0 +1,75 @@
+import { NEWS_CATEGORIES } from "./categories";
+
+export const SOURCES = {
+  [NEWS_CATEGORIES.TECHNOLOGY]: [
+    "https://techcrunch.com",
+    "https://thenextweb.com",
+    "https://www.theverge.com",
+    "https://www.wired.com",
+    "https://arstechnica.com",
+    "https://www.zdnet.com",
+  ],
+
+  [NEWS_CATEGORIES.AI]: [
+    "https://venturebeat.com/category/ai/",
+    "https://www.technologyreview.com/topic/artificial-intelligence/",
+    "https://openai.com/blog",
+    "https://ai.googleblog.com",
+    "https://huggingface.co/blog",
+    "https://deepmind.google/discover/blog",
+  ],
+
+  [NEWS_CATEGORIES.BUSINESS]: [
+    "https://www.bloomberg.com",
+    "https://www.forbes.com/business/",
+    "https://www.businessinsider.com",
+    "https://www.ft.com",
+    "https://www.cnbc.com/business/",
+    "https://www.reuters.com/business",
+  ],
+
+  [NEWS_CATEGORIES.SPORTS]: [
+    "https://www.espn.com",
+    "https://www.skysports.com",
+    "https://www.bbc.com/sport",
+    "https://www.sportskeeda.com",
+    "https://www.cbssports.com",
+    "https://www.goal.com",
+  ],
+
+  [NEWS_CATEGORIES.WORLD]: [
+    "https://www.bbc.com/news/world",
+    "https://www.aljazeera.com",
+    "https://www.reuters.com/world",
+    "https://edition.cnn.com/world",
+    "https://www.dw.com/en/top-stories/s-9097",
+    "https://apnews.com/world-news",
+  ],
+
+  [NEWS_CATEGORIES.POLITICS]: [
+    "https://www.politico.com",
+    "https://www.reuters.com/politics",
+    "https://www.bbc.com/news/politics",
+    "https://thehill.com",
+    "https://www.npr.org/sections/politics/",
+    "https://apnews.com/politics",
+  ],
+
+  [NEWS_CATEGORIES.STARTUPS]: [
+    "https://techcrunch.com/startups/",
+    "https://www.ycombinator.com/blog",
+    "https://www.eu-startups.com",
+    "https://www.failory.com/blog",
+    "https://startupnation.com",
+    "https://sifted.eu",
+  ],
+
+  [NEWS_CATEGORIES.HEALTH]: [
+    "https://www.healthline.com",
+    "https://www.who.int/news",
+    "https://www.medicalnewstoday.com",
+    "https://www.webmd.com",
+    "https://www.nih.gov/news-events",
+    "https://www.cdc.gov/media",
+  ],
+};
